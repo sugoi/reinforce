bc2b4fdae1cf0bb8a5afe390df12b1486064fe8b
Takayuki Muranushi <muranushi@gmail.com>
2013-03-09 20:45:21 +0900

    start implementing

--- Control/Monad/Rein/Class.hs
+++ Control/Monad/Rein/Class.hs
@@ -1,2 +1,1 @@
 module Control.Monad.Rein.Class where
-
--- Control/Monad/Trans/Rein.hs
+++ Control/Monad/Trans/Rein.hs
@@ -1,1 +1,17 @@
 module Control.Monad.Trans.Rein where
+
+
+import qualified Control.Monad.State as State
+import qualified Control.Monad.State.Class as State
+import           Data.Typeable
+import qualified Data.Binary as Bin
+import qualified Data.ByteString.Lazy.Char8 as BS
+
+data Context
+  = ContextString  BS.ByteString
+  | ContextTypeRep TypeRep
+  | ContextRepeat  Int Context
+
+data ReinState = ReinState
+
+type ReinT m a = State.StateT ReinState m a
